021d50c0fad770aaacb05fca4bc54959d9c38dbd
Bailey <bailey.g.weathers@gmail.com>
2016-06-23 11:40:49 -0500

    Features address-bloc checkpoint-19 problem

---
+++ models/entry.rb
@@ -0,0 +1,21 @@
+
+# checkpoint 19
+
+class Entry
+
+     attr_accessor      :name, :phone_number, :email
+     
+     
+     
+     
+    def initialize(name, phone_number, email)
+
+            #@name, @phone_number, @email = name, phone_number, email
+
+    end
+
+     
+     
+end
+
+
