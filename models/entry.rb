acecbc22d8e78c5781be06e0d0ded4c12b74b4f3
Bailey <bailey.g.weathers@gmail.com>
2016-06-23 16:18:39 -0500

    Features Exercise in checkpoint 19 complete

--- models/entry.rb
+++ models/entry.rb
@@ -3,19 +3,24 @@
 
 class Entry
 
-     attr_accessor      :name, :phone_number, :email
-     
-     
-     
-     
+    attr_accessor      :name, :phone_number, :email
+
+
+
+
     def initialize(name, phone_number, email)
 
-            #@name, @phone_number, @email = name, phone_number, email
+            @name, @phone_number, @email = name, phone_number, email
 
     end
 
-     
-     
+    def to_s
+
+     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+
+    end
+
+
+
+
 end
-
-
