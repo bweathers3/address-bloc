210a33a21d3e611c0d83a4f0bd5ad34798f97f6a
Bailey <bailey.g.weathers@gmail.com>
2016-06-25 16:11:30 -0500

    Final search for Checkpoint 22

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,7 +44,7 @@ class AddressBook
               add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
             end
        end
-
+###########
 
        def remove_entry(name, phone_number, email)
 
@@ -63,5 +63,67 @@ class AddressBook
          end
 
       end
+###########
+
+   # Search AddressBook for a specific entry by name
+   def binary_search(name)
+
+
+      lower = 0
+      upper = entries.length - 1
+
+
+      while lower <= upper
+
+        mid = (lower + upper) / 2
+        mid_name = entries[mid].name
+
+
+        if name == mid_name
+          return entries[mid]
+          elsif name < mid_name
+            upper = mid - 1
+          elsif name > mid_name
+            lower = mid + 1
+          end
+        end
+
+
+        return nil
+
+   end
+
+
+##############
+
+   # Search AddressBook for a specific entry by name
+
+   def iterative_search(name)
+
+
+           lower = 0
+           upper = entries.length - 1
+
+           while lower <= (upper)
+
+             if  entries[lower].name == name
+
+               return entries[lower]
+
+             else
+
+               lower += 1
+
+             end
+
+           end
+
+             return nil
+
+
+   end
+
+##############
+
 
 end
